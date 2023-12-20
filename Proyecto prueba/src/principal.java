1ddaf4dca6a6bdf6e69c1454ed5c9d2ccd23f0b1
Toryzeus <toryzeus@gmail.com>
2023-12-20 17:50:23 +0100

    Subida final a GitHud

--- Proyecto prueba/src/principal.java
+++ Proyecto prueba/src/principal.java
@@ -5,6 +5,7 @@ public class principal {
 		System.out.println("Holi Wankenobi");
 		System.out.println("Agur Yogur!");
 		System.out.println("Ciao pescao");
+		System.out.println("Adios de verdad!");
 
 	}
 
