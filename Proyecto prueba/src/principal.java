5aaae80c274a34c7a846faf12f309c839ac417ab
Toryzeus <153745236+Toryzeus@users.noreply.github.com>
2023-12-20 17:35:29 +0100

    Incluyo una forma ingeniosa de despedirse
    Ciao pescao

--- Proyecto prueba/src/principal.java
+++ Proyecto prueba/src/principal.java
@@ -3,6 +3,8 @@ public class principal {
 
 	public static void main(String[] args) {
 		System.out.println("Holi Wankenobi");
+		System.out.println("Agur Yogur!");
+		System.out.println("Ciao pescao");
 
 	}
 
